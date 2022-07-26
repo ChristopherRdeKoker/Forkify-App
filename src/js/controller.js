48ec1414b034e3eb78be95d0fddbdc911ff9a4b3
ChristopherRdeKoker <108460798+ChristopherRdeKoker@users.noreply.github.com>
2022-07-26 19:25:39 +0200

    Search to console funct
    Part 1: Implemented search functionality to console;

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,26 +1,16 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
-// ('use strict');
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-// console.log(icons);
-//shortcuts
-
-// const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-///////////////////////////////////////
-//functions
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
-
     //1) loading recipe
     await model.loadRecipe(id);
     // const { recipe } = model.state;
@@ -33,7 +23,20 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    await model.loadSearchResults(query);
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
