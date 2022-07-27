7c0528e0fb1002aa6d825f8efb9f994539bffe7a
ChristopherRdeKoker <108460798+ChristopherRdeKoker@users.noreply.github.com>
2022-07-27 11:38:14 +0200

    Fin Search feature
    Created View.js file to oversee parental public functions and vars;
    fully implemented search feature;
    coming up = limit search to 10-per page, & pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,15 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -18,18 +24,23 @@ const controlRecipes = async function () {
     // 2 rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    // console.log(err);
     recipeView.renderError();
   }
 };
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
+    //1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
 
+    //2) Load search results
     await model.loadSearchResults(query);
-    console.log(model.state.search.results);
+
+    //3) Render results
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
