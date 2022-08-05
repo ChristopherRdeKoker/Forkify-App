82accf555b114061e57da90bdc388d4b7cd92994
ChristopherRdeKoker <108460798+ChristopherRdeKoker@users.noreply.github.com>
2022-08-05 10:25:15 +0200

    Uploading a new recipe P1

--- src/js/model.js
+++ src/js/model.js
@@ -111,3 +111,8 @@ const init = function () {
   if (storage) state.bookmarks = JSON.parse(storage);
 };
 init();
+
+const clearBookmarks = function () {
+  localStorage.clear('bookmarks');
+};
+// clearBookmarks();
