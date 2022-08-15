e214e016e9ad5c06eeead3f2e0268ff2c111a332
ChristopherRdeKoker <christopherdekoker@gmail.com>
2022-08-15 13:42:33 +0200

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,6 +124,9 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const hiThere = function () {
+  console.log(`Welcome to the application!`);
+};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -132,5 +135,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  hiThere();
 };
 init();
